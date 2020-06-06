68abc7c285220f533811171dbcd91a73d8e1bc08
Pawndev <coquelet.c@gmail.com>
2020-06-07 00:22:41 +0200

    feat(history_stats): Export graph per task
    Export per task response_times and req_per_second

--- locuplot/locust/history_stats.py
+++ locuplot/locust/history_stats.py
@@ -9,9 +9,35 @@ class HistoryStats:
         self.df = pd.read_csv(history_filepath, sep=",")
         self.df["datetime"] = self.df["Timestamp"].apply(lambda t: datetime.fromtimestamp(t))
 
-    def generate_history_plot(self):
-        ax1 = plt.gca()
-        self.df.plot(x="datetime", y="Requests/s", ax=ax1)
-        self.df.plot(x="datetime", y="Failures/s", ax=ax1)
+    def generate_response_times_plot(self, df):
+        plt.figure()
+        ax = plt.gca()
+        df.plot(x="datetime", y="Total Median Response Time", ax=ax)
+        df.plot(x="datetime", y="95%", ax=ax)
+        df.plot(x="datetime", y="75%", ax=ax)
+        df.plot(x="datetime", y="50%", ax=ax)
         fig = plt.gcf()
         return fig
+
+    def generate_req_per_sec_plot(self, df):
+        plt.figure()
+        ax = plt.gca()
+        df.plot(x="datetime", y="Requests/s", ax=ax, color="blue")
+        df.plot(x="datetime", y="Failures/s", ax=ax, color="red")
+        fig = plt.gcf()
+        return fig
+
+    def generate_all_req_per_sec_plot(self):
+        """
+        Generate all figures
+        :return: Array<{name: string, figure: Figure}>
+        """
+        tasks_name = self.df.Name.unique()
+        return [
+            {
+                "name": task,
+                "req_per_sec_figure": self.generate_req_per_sec_plot(self.df[self.df.Name == task]),
+                "response_times_figure": self.generate_response_times_plot(self.df[self.df.Name == task])
+            } for task in tasks_name
+        ]
+
--- locuplot/main.py
+++ locuplot/main.py
@@ -1,18 +1,33 @@
+import os
 from .argument_parser import parse_args, validate_args, format_args
 from .locust.stats import Stats
 from .helpers.fs import create_dir
 from .helpers.plot import save_plot
+
 
 def main():
     args = parse_args()
     validate_args(args)
     formatted_args = format_args(args)
     create_dir(formatted_args.export_dir)
+    create_dir(os.path.join(formatted_args.export_dir, 'req_per_second'))
+    create_dir(os.path.join(formatted_args.export_dir, 'response_times'))
     stats = Stats(
         history_filepath=formatted_args.history_filepath,
         stats_filepath=formatted_args.stats_filepath,
         failures_filepath=formatted_args.failures_filepath
     )
-    history_figure = stats.history.generate_history_plot()
-    save_plot(history_figure, formatted_args.export_dir, 'global_req_per_second.png', 'png')
-
+    history_figures = stats.history.generate_all_req_per_sec_plot()
+    for history_task in history_figures:
+        save_plot(
+            history_task.get('req_per_sec_figure'),
+            os.path.join(formatted_args.export_dir, 'req_per_second'),
+            '{}.png'.format(history_task.get('name')),
+            'png'
+        )
+        save_plot(
+            history_task.get('response_times_figure'),
+            os.path.join(formatted_args.export_dir, 'response_times'),
+            '{}.png'.format(history_task.get('name')),
+            'png'
+        )
